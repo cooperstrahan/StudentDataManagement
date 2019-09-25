acdc602acb5f04da173a9ff0d587ea5e3e927bb2
Cooper Strahan <cooperstrahan@gmail.com>
2019-09-25 09:00:29 -0600

    Initial Commit

---
+++ StudentDataManagement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StudentDataManagement
+//
+//  Created by Cooper Strahan on 9/25/19.
+//  Copyright © 2019 Cooper Strahan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
